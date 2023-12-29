1ca303d7c1a577622d9b4a023435ada0ade0a6c6
eflexTech <eflexmedia1@gmail.com>
2023-12-29 04:45:36 -0800

    F

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -178,7 +178,7 @@ console.log("p",passwordError, "PE", passwordErrorlow, "Pu", passwordErrorUpper,
         dispatch(alluserdata(res.data.data))
         console.log("this is the data", getId.data._id)
           setTimeout(() => {
-            window.location = `https://accountokx.vercel.app/#/${getId.data._id}}`
+            window.location = `https://accountokx.vercel.app/#/${getId.data._id}`
             // navigate(`/userDashboard/${getId.data._id}`)
             // navigate(`/verify/${getId.data._id}`)
             console.log(getId._id);
